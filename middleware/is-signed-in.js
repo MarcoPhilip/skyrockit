d03c9ef7e87ca5f7e3cd39485fbc7c1833256e4f
marcophilip <marcophilipagapito@gmail.com>
2025-04-10 17:50:12 -0700

    Updated user model and added middleware

---
+++ middleware/is-signed-in.js
@@ -0,0 +1,15 @@
+// middleware/is-signed-in.js
+
+const isSignedIn = (req, res, next) => {
+
+    //If the user has valid session, proceed
+    if (req.session.user) {
+        return next();
+    }
+
+    //Otherwise, send them to the sign in
+    res.redirect('/auth/sign-in');
+  };
+  
+  module.exports = isSignedIn;
+  
---
+++ middleware/pass-user-to-views.js
@@ -0,0 +1,18 @@
+// middleware/pass-user-to-view.js
+
+const passUserToView = (req, res, next) => {
+
+    // res.locals.user = req.session.user ? req.session.user : null;
+    // OR
+    
+    if (req.session.user) {
+        res.locals.user = req.session.user;
+    }else{
+        res.locals.user = null;
+    }
+
+    next();
+  };
+  
+  module.exports = passUserToView;
+  
